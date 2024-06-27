93efe812e6d7ad3c536bbd120e2598c8f3c2044f
RC-AJAY <124486151+RC-AJAY@users.noreply.github.com>
2024-06-27 16:12:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 docker login -u rc2024  -p dckr_pat_DUWoNZlOYJlF7Wv7-_7tG6KlTVo
 
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
-       sh'chmod +x build.sh'
-       sh'./build.sh'
+       sh 'chmod +x build.sh'
+       sh './build.sh'
        docker-compose up -d
 
         docker tag test rc2024/dev
         docker push rc2024/dev
 
-if [[ $GIT_BRANCH == "origin/master" ]]; then
-        sh'chmod +x build.sh'
-        sh'./build.sh'
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
+        sh 'chmod +x build.sh'
+        sh './build.sh'
         docker-compose up -d
 
         docker tag test rc2024/prod
